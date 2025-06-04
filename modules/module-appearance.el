0654c1a086e2d483b3458bc2cb27c207745d17dc
Evelynn Straw <evelynn@evelynn.me>
2025-06-04 13:31:32 -0700

    Avoid unbinding key for org-roam capture

--- modules/module-appearance.el
+++ modules/module-appearance.el
@@ -34,8 +34,6 @@ of a light theme."
 (push '("\\`\\*e?shell" display-buffer-pop-up-window) display-buffer-alist)
 
 (use-package dashboard
-  :init
-  (global-unset-key (kbd "C-x C-n"))
   :config
   (dashboard-setup-startup-hook)
   (setq dashboard-center-content t)
